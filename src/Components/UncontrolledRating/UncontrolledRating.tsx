1215a73265f3c5f8814203c5ecdd29fd5c4e556a
Ivan Kevra <78702646+ivankevra@users.noreply.github.com>
2022-12-17 16:48:37 +0300

    3rd lesson=

--- src/Components/UncontrolledRating/UncontrolledRating.tsx
+++ src/Components/UncontrolledRating/UncontrolledRating.tsx
@@ -2,15 +2,14 @@ import React, {useState} from "react";
 
 
 export const UncontrolledRating = () => {
-let [value, setValue] = useState(0)
-
+    let [value, setValue] = useState(0)
     return (
         <div>
-            <Star selected={value > 0}/> <button onClick={(value)=>{setValue(1)}}>1</button>
-            <Star selected={value > 1}/> <button onClick={(value)=>{setValue(2)}}>2</button>
-            <Star selected={value > 2}/> <button onClick={(value)=>{setValue(3)}}>3</button>
-            <Star selected={value > 3}/> <button onClick={(value)=>{setValue(4)}}>4</button>
-            <Star selected={value > 4}/> <button onClick={(value)=>{setValue(5)}}>5</button>
+            <Star selected={value > 0}/><button onClick={() => {setValue(1)}}>1</button>
+            <Star selected={value > 1}/><button onClick={() => {setValue(2)}}>2</button>
+            <Star selected={value > 2}/> <button onClick={() => {setValue(3)}}>3</button>
+            <Star selected={value > 3}/> <button onClick={() => {setValue(4)}}>4</button>
+            <Star selected={value > 4}/> <button onClick={() => {setValue(5)}}>5</button>
         </div>
     )
 }
@@ -20,9 +19,11 @@ type StarPropsType = {
 }
 
 const Star = (props: StarPropsType) => {
-    if (props.selected === true) {
-        return <span><b>Star </b></span>
-    } else {
-        return <span>Star </span>
+    const onClickHandler = () => {
+
     }
+    return props.selected
+        ? <span onClick={onClickHandler}><b>Star </b></span>
+        : <span onClick={onClickHandler}>Star </span>
+
 }
