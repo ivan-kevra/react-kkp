ab434f438c25230bc3657020518b3243940c8786
ivan-kevra <vanyakevra@gmail.com>
2023-03-14 15:54:33 +0300

    1st

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,31 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import {Rating, RatingPropsType} from "./Components/Rating/Rating";
-import {OnOff} from "./Components/OnOff/OnOff";
-import {Accordion} from "./Components/Accordion/Accordion";
-import {UncontrolledAccordion} from "./Components/UncontrolledAccordion/UncontrolledAccordion";
-import {UncontrolledRating} from "./Components/UncontrolledRating/UncontrolledRating";
+import {Accordion} from "./components/accordion/Accordion";
+import {Rating} from "./components/rating/Rating";
 
-export type ValuesType = 0 | 1 | 2 | 3 | 4 | 5
 
 const App = () => {
-
-    let [value, setValue] = useState<ValuesType>(0)
-    let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
-    let [on, setOn] = useState(false)
     return (
         <div>
-            <Rating value={value} onClick={setValue}/>
-            <UncontrolledRating/>
-            <Accordion
-                titleValue={'Menu'}
-                collapsed={accordionCollapsed}
-                onChange={setAccordionCollapsed}
-            />
-            <OnOff on={on} onChange={setOn} />
+            <PageTitle title={'This is APP component'}/>
+            <PageTitle title={'My friends'}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion title={'Меню'} collapsed={true}/>
+            <Accordion title={'Users '} collapsed={false}/>
+            Article 2
+            <Rating value={4}/>
         </div>
     );
 }
+
+export default App;
+
 
 type PageTitlePropsType = {
     title: string
 }
 
 const PageTitle = (props: PageTitlePropsType) => {
-    return <div>{props.title}</div>
+    return <h1>{props.title}</h1>
 }
-
-export default App;
-
---
+++ src/components/rating/Rating.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+
+type RatingPropsType = {
+    value: 0 | 1 | 2 | 3 | 4 | 5
+}
+
+export const Rating = (props: RatingPropsType) => {
+    if (props.value === 1) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 2) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 3) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 4) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+            </div>
+        )
+    }
+    if (props.value === 5) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+        </div>
+    )
+}
+
+type StarPropsType = {
+    selected: boolean
+}
+const Star = (props: StarPropsType) => {
+    return <span>{props.selected ? <b>Star </b> : 'Star '}</span>
+
+}
